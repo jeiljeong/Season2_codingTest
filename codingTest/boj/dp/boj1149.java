dd46fccf710f65189d97a2ed074a7025ced3952d
JeilJeong <wjdwpdlf12@gmail.com>
2022-09-03 17:51:20 +0900

    boj1149.java solved by using iteration

--- codingTest/boj/dp/boj1149.java
+++ codingTest/boj/dp/boj1149.java
@@ -1,8 +1,52 @@
 package codingTest.boj.dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * cf.
  */
 public class boj1149 {
-    
+    private static int n;
+    private static int[] dp;
+    private static int[][] rgb;
+    private static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // 1. Check input validation & initialization
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        n = Integer.parseInt(br.readLine());
+        
+        dp = new int[n];
+        rgb = new int[n][3];
+        
+        StringTokenizer st = null;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                rgb[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 2. Biz logic
+        rgbSelect(0,0, 0);
+        
+        
+        // 3. End
+        
+    }
+
+    private static void rgbSelect(int avoidRgb, int depth, int sum) {
+        
+        for (int i = 0; i < 3; i++) {
+            if (i == avoidRgb)
+                continue;
+            sum += rgb[depth][i];
+            min = Math.min(sum, min);
+            rgbSelect(i, depth + 1, sum);
+            sum -= rgb[depth][i];
+        }
+    }
 }
---
+++ codingTest/boj/dp/boj1149_v2.java
@@ -0,0 +1,39 @@
+package codingTest.boj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1149_v2 {
+    private final static int RED = 0;
+    private final static int GREEN = 1;
+    private final static int BLUE = 2;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // 1. Check input validation & initialization
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] cost = new int[n][3];
+        
+        StringTokenizer st = null;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 2. Biz logic
+        for (int i = 1; i < n; i++) {
+            cost[i][RED]    += Math.min(cost[i - 1][GREEN]  , cost[i - 1][BLUE]);
+            cost[i][GREEN]  += Math.min(cost[i - 1][RED]    , cost[i - 1][BLUE]);
+            cost[i][BLUE]   += Math.min(cost[i - 1][RED]    , cost[i - 1][GREEN]);
+        }
+        
+        // 3. End
+        int answer = Math.min(cost[n - 1][RED], cost[n - 1][GREEN]);
+        answer = Math.min(cost[n - 1][BLUE], answer);
+        System.out.println(answer);
+    }
+}

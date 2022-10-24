7b893c6f054778c2e4916ff9a5374530c27b5609
JeilJeong <wjdwpdlf12@gmail.com>
2022-10-24 21:05:36 +0900

    boj11659.java

---
+++ codingTest/boj/accumSum/boj12865.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj12865 {
+    private static Integer[] dp;
+    private static int[] num;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // 1. Check input validation & initialization
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        num = new int[n + 1];
+        dp = new Integer[n - k + 2];
+        dp[0] = 0;
+        dp[1] = 0;
+        
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 2. Biz logic
+        for (int i = 1; i <= k; i++) {
+            dp[1] += num[i];
+        }
+        System.out.println(dp[1]);
+        for (int i = 2; i <= k; i++) {
+            dp[i] = dp[i - 1] + num[i + k - 1] - num[i - 1];
+        }        
+        
+        // 3. End
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= k; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
